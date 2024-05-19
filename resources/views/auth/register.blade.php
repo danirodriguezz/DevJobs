258db1d4a471fb1b9c7844f9c929cf1fd0bc2927
Daniel Rodriguez <danielrodriguezalonso02@gmail.com>
2024-05-19 13:02:57 +0100

    Adding authentication logic with Breeze

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -14,6 +14,21 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- User Rol -->
+        <div class="mt-4">
+            <x-input-label for="rol" :value="__('¿Que tipo de cuenta deseas en DevJobs?')" />
+            <select 
+                name="rol" 
+                id="rol"
+                class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+            >
+                <option value=""> ---- Selecciona una opción ----</option>
+                <option value="1">Developer - Obtener un empleo</option>
+                <option value="2">Reclutador - Publicar un empleo</option>
+            </select>
+            <x-input-error :messages="$errors->get('rol')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -30,7 +45,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Repetir Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -39,14 +54,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
+        <div class="flex justify-between my-5">
+            <x-link :href="route('password.request')">
+                Olvidaste tu password
+            </x-link>
+            <x-link :href="route('login')">
+                Iniciar Sesión
+            </x-link>
+        </div>        
+        <x-primary-button class="w-full justify-center">
+            {{ __('Crear Cuenta') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
